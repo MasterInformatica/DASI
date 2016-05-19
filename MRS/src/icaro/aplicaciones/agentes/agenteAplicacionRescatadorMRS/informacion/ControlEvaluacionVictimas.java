50f8f17b67214f7f0374bfbc6f72eb65343980a7
Hristo Ivanov <hristo.idgh@gmail.com>
2016-05-19 11:33:19 +0200

    JavaDocs. AgenteRescatador. Información. ControlEvaluacionVictimas.

--- MRS/src/icaro/aplicaciones/agentes/agenteAplicacionRescatadorMRS/informacion/ControlEvaluacionVictimas.java
+++ MRS/src/icaro/aplicaciones/agentes/agenteAplicacionRescatadorMRS/informacion/ControlEvaluacionVictimas.java
@@ -4,23 +4,45 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.TreeSet;
 
+/**
+ * Clase que controla la evaluacion y asignacion de las victimas por parte de los rescatadores
+ * @author Luis Maria Costero Valero
+ */
 public class ControlEvaluacionVictimas {
-	//Control de las victimas evaluadas y asignadas
+
+	/**
+	 * La proxima victima a ser asignada a un robot.
+	 */
 	public String proximaVictima;
+	
+	/**
+	 * El numero de victimas evaluadas.
+	 */
 	private int victimasEvaluadas = 0;
+	
+	/**
+	 * Lista de todas las victimas que necesitan ser rescatadas.
+	 */
 	public List<String> victimasArescatar;
+	
+	/**
+	 * Valor booleano que indica que se han evaluado todas las victimas.
+	 */
 	public boolean finalizadaTodasEvaluaciones;
 	
-	//Control de los robots ya asignados
+	/**
+	 * Control de los robots ya asignados al rescate de una victima.
+	 */
 	public Map<String, Boolean> robotsAsignados;
 
 
-	
+	/**
+	 * Constructora de la clase.
+	 * @param robots Lista de los agentes rescatadores.
+	 */
 	public ControlEvaluacionVictimas(List<String> robots){
 		//mineros
 		this.victimasArescatar = new ArrayList<String>();
@@ -35,20 +57,19 @@ public class ControlEvaluacionVictimas {
 		}
 	}
 	
-	
-	public boolean isFinalizadaTodasEvaluaciones() {
-		return finalizadaTodasEvaluaciones;
-	}
-
-	public boolean getFinalizadaTodasEvaluaciones() {
-		return finalizadaTodasEvaluaciones;
-	}
-
-
+	/**
+	 * Funcion que nos consultar la proxima victima a ser asignada.
+	 * @return Identificador de la proxima victima a ser asignada.
+	 */
 	public String getProximaVictima() {
 		return proximaVictima;
-	}	
+	}
 	
+
+	/**
+	 * Funcion que nos permite anadir una victima a la lista de victimas a ser rescatadas.
+	 * @param s Identificador de la victima.
+	 */
 	public void addVictima(String s){
 		this.victimasArescatar.add(s);
 			
@@ -58,28 +79,50 @@ public class ControlEvaluacionVictimas {
 		this.proximaVictima = this.victimasArescatar.get(0);
 	}
 	
+	/**
+	 * Funcion que nos permite eliminar una victima de la lista de victimas a ser rescatadas.
+	 * Esta función se invoca al asignar un rescatador a la victima.
+	 * @param s Identificador de la victima.
+	 */
 	public void eliminaVictima(String s){
 		this.victimasArescatar.remove(s);
 		if(this.victimasArescatar.size() != 0)
 			this.proximaVictima = victimasArescatar.get(0);
 	}
 	
-
+	/**
+	 * Funcion que nos permite marcar como finalizada la evaluacion de una victima por parte de todos los robots.
+	 * Esta función se invoca al asignar un rescatador a la victima.
+	 * @param minero Identificador de la victima.
+	 */
 	public void informarEvaluacionFinalizada(String minero) {
 		victimasEvaluadas ++;
 		finalizadaTodasEvaluaciones = victimasEvaluadas == this.victimasArescatar.size();
 	}
 	
-	
+	/**
+	 * Funcion que nos permite marcar como asignado a un agente rescatador.
+	 * Esta función se invoca al asignar un rescatador a la victima.
+	 * @param robotId Identificador del agente rescatador.
+	 */
 	public void setRobotAsignado(String robotId){
 		this.robotsAsignados.put(robotId, true);
 	}
 	
+	/**
+	 * Funcion que nos permite marcar como No asignado a un agente rescatador.
+	 * Esta función se invoca cuando un robot termina el rescate de una victima.
+	 * @param robotId Identificador del agente rescatador.
+	 */
 	public void unsetRobotAsignado(String robotId){
 		this.robotsAsignados.put(robotId, false);
 	}
 	
-	
+	/**
+	 * Funcion que nos permite consultar si un agente rescatador es asignado o no.
+	 * @param s Identificador del agente rescatador.
+	 * @return Verdadero si el robot esta asignado. Falso en caso contrario.
+	 */
 	public boolean isRobotAsigned(String s){
 		Boolean b = this.robotsAsignados.get(s);
 		if(b== null)
@@ -89,11 +132,18 @@ public class ControlEvaluacionVictimas {
 
 	}
 
+	/**
+	 * Funcion que nos permite consultar el numero de victimas a rescatar.
+	 * @return El numero de victimas a rescatar.
+	 */
 	public int getNumVictimasARescatar(){
 		return this.victimasArescatar.size();
 	}
 
-
+	/**
+	 * Funcion que nos permite consultar el numero de rescatadores que tienen una victima asignada.
+	 * @return El numero de rescatadores que tienen una victima asignada.
+	 */
 	public int getNumRobotsOcupados() {
 		int total = 0;
 		
@@ -104,6 +154,4 @@ public class ControlEvaluacionVictimas {
 		
 		return total;
 	}
-	
-
 }
